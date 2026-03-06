a5f3f52e51fc9efe3a81b78e1cadeeb6c4234616
Jiayi Wei <wjydzh1@gmail.com>
2026-03-06 00:01:07 +0000

    fix(types): cover shell-quote glob token shape

--- web/server/shell-quote.d.ts
+++ web/server/shell-quote.d.ts
@@ -1,5 +1,9 @@
 declare module "shell-quote" {
-  export type ParseToken = string | { op: string } | { comment: string };
+  export type ParseToken =
+    | string
+    | { op: string }
+    | { comment: string }
+    | { pattern: string };
 
   export function parse(input: string): ParseToken[];
 }
